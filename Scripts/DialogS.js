49ec4b36f652fe2bae85ac3292942eb9462c0666
Arion <ccina0156@gmail.com>
2024-06-03 19:21:52 +0200

    Create DialogS.js

---
+++ Scripts/DialogS.js
@@ -0,0 +1,5 @@
+// DialogS.js
+// La idea es hacer algo como
+// Nombre del personaje está feliz a la derecha
+// Nombre del personaje se desliza a la izquierda
+// Nombre del personaje está serio en el centro
