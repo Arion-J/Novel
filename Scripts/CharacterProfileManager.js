1a553cc9a31f0584be6a7391a60b611c96e61c8f
Arion <ccina0156@gmail.com>
2024-06-03 18:33:50 +0200

    Resumen dinamico

--- Scripts/CharacterProfileManager.js
+++ Scripts/CharacterProfileManager.js
@@ -1,15 +1,17 @@
 const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes';
 
 (async () => {
-  try {
-    const response = await fetch(repoUrl);
-    const folders = (await response.json()).filter(item => item.type === 'dir');
-    const selectElement = document.getElementById('selectpj');
-    folders.forEach(({ name }) => {
-      const option = new Option(name, name);
-      selectElement.appendChild(option);
-    });
-  } catch (error) {
-    console.error('Error al obtener las carpetas:', error);
-  }
+  const response = await fetch(repoUrl);
+  const folders = (await response.json()).filter(item => item.type === 'dir');
+  const selectElement = document.getElementById('selectpj');
+  
+  folders.forEach(({ name }) => {
+    selectElement.add(new Option(name, name));
+  });
+
+  selectElement.addEventListener('change', async ({ target: { value } }) => {
+    const characterContentUrl = `https://arion-j.github.io/Novel/Personajes/${value}/`;
+    const contentResponse = await fetch(characterContentUrl);
+    document.getElementById('resumepj').innerHTML = await contentResponse.text();
+  });
 })();
