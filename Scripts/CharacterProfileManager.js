27761e59b91a4aaaff1bf1dc79e8b19138068ca8
Arion <ccina0156@gmail.com>
2024-06-03 16:45:23 +0200

    CharacterProfileManager.js corregido

--- Scripts/CharacterProfileManager.js
+++ Scripts/CharacterProfileManager.js
@@ -1,28 +1,15 @@
-// CharacterProfileManager.js
+const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes';
 
-// Función para obtener las carpetas dentro de la carpeta Personajes
-function obtenerCarpetasPersonajes() {
-    // Array para almacenar los nombres de las carpetas
-    const carpetas = [];
-
-    // Encontrar todos los enlaces que apuntan a carpetas dentro de la carpeta Personajes
-    const enlacesCarpetas = document.querySelectorAll('a[href^="/Novel/Personajes/"]');
-    enlacesCarpetas.forEach(enlace => {
-        // Obtener la parte de la URL después de "/Personajes/"
-        const ruta = enlace.getAttribute('href').substring('/Novel/Personajes/'.length);
-        // Obtener el nombre de la carpeta
-        const nombreCarpeta = ruta.split('/')[0];
-        // Añadir el nombre de la carpeta al array si no está presente
-        if (!carpetas.includes(nombreCarpeta)) {
-            carpetas.push(nombreCarpeta);
-        }
+(async () => {
+  try {
+    const response = await fetch(repoUrl);
+    const folders = (await response.json()).filter(item => item.type === 'dir');
+    const selectElement = document.getElementById('selectpj');
+    folders.forEach(({ name }) => {
+      const option = new Option(name, name);
+      selectElement.appendChild(option);
     });
-
-    return carpetas;
-}
-
-// Llamar a la función para obtener las carpetas cuando se cargue el script
-window.addEventListener('DOMContentLoaded', function() {
-    const carpetas = obtenerCarpetasPersonajes();
-    console.log('Carpetas dentro de Personajes:', carpetas);
-});
+  } catch (error) {
+    console.error('Error al obtener las carpetas:', error);
+  }
+})();
