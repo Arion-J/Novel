e6f140245dcf4274a3ea59e2075d21e84f21379a
Arion <ccina0156@gmail.com>
2024-06-03 18:40:29 +0200

    Actualiza el select correctamente

--- Scripts/CharacterProfileManager.js
+++ Scripts/CharacterProfileManager.js
@@ -9,9 +9,18 @@ const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes'
     selectElement.add(new Option(name, name));
   });
 
-  selectElement.addEventListener('change', async ({ target: { value } }) => {
-    const characterContentUrl = `https://arion-j.github.io/Novel/Personajes/${value}/`;
+  const loadCharacterContent = async (characterName) => {
+    const characterContentUrl = `https://arion-j.github.io/Novel/Personajes/${characterName}/`;
     const contentResponse = await fetch(characterContentUrl);
     document.getElementById('resumepj').innerHTML = await contentResponse.text();
+  };
+
+  // Load the content of the first character by default
+  if (folders.length > 0) {
+    loadCharacterContent(folders[0].name);
+  }
+
+  selectElement.addEventListener('change', ({ target: { value } }) => {
+    loadCharacterContent(value);
   });
 })();
