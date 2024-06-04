7609d1949f195bb8ab7fbf890e2717d71d68bbed
Arion <ccina0156@gmail.com>
2024-06-04 22:49:31 +0200

    Importar variable nombres de personajes

--- Scripts/DialogS.js
+++ Scripts/DialogS.js
@@ -1,7 +1,4 @@
-import { getCharacterNames } from '/Scripts/CharacterProfileManager.js';
-
+const {characterNames} = require('/Scripts/CharacterProfileManager.js');
 document.addEventListener('DOMContentLoaded', async () => {
-  const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes';
-  const characterNames = await getCharacterNames(repoUrl);
   console.log(characterNames);
 });
