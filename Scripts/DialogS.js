cba1ee0be71f551d582e799e555bfa9b21b7b003
Arion <ccina0156@gmail.com>
2024-06-04 16:47:37 +0200

    Update DialogS.js

--- Scripts/DialogS.js
+++ Scripts/DialogS.js
@@ -1,25 +1,9 @@
-// Importa la clase CharacterProfileManager
-import CharacterProfileManager from './CharacterProfileManager.js';
+import characterProfileManager from './CharacterProfileManager.js';
 
-// Crear una función para cargar y mostrar los nombres de los personajes
-async function showCharacterNames() {
-  // URL del repositorio donde se encuentran los personajes
-  const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes';
-
-  // Crear una instancia de CharacterProfileManager
-  const characterProfileManager = new CharacterProfileManager(null, null, repoUrl);
-
-  // Inicializar el objeto para cargar los nombres de los personajes
-  await characterProfileManager.loadCharacterFolders();
-
-  // Obtener los nombres de los personajes
+// Muestra los nombres de los personajes en la consola
+function showCharacterNames() {
   const characterNames = characterProfileManager.getCharacterNames();
-
-  // Crear el texto con los nombres de los personajes
-  const characterNamesText = characterNames.join(', '); // Unir los nombres con una coma y un espacio
-
-  // Mostrar el texto en el cuerpo del documento
-  document.body.textContent += characterNamesText;
+  console.log(characterNames);
 }
 
 // Ejecutar la función para mostrar los nombres de los personajes cuando el DOM esté completamente cargado
