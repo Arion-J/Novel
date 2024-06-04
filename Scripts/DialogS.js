48cb35da29562a3f79a1ea990e5a6f038863c421
Arion <ccina0156@gmail.com>
2024-06-04 22:59:18 +0200

    Enlace correcto a Profile Manager

--- Scripts/DialogS.js
+++ Scripts/DialogS.js
@@ -1,4 +1,4 @@
-const {characterNames} = require('/Scripts/CharacterProfileManager.js');
+const {characterNames} = require('https://raw.githubusercontent.com/Arion-J/Novel/main/Scripts/CharacterProfileManager.js');
 document.addEventListener('DOMContentLoaded', async () => {
   console.log(characterNames);
 });
