f05ae31673361d70a7b301e2b875271539946b68
Arion <ccina0156@gmail.com>
2024-06-13 21:56:46 +0200

    Update dialog.js

--- Scripts/dialog.js
+++ Scripts/dialog.js
@@ -1,4 +1,7 @@
-import { getCharacterNames } from './getCharacterNames.js';
+import CharacterProfileManager from './characterProfileManager.js';
+const characterProfileManager = new CharacterProfileManager(selectElementId, resumeElementId, repoUrl);
+characterProfileManager.init();
+const characterNames = characterProfileManager.getCharacterNames();
 console.log("Nombres de personajes:");
 characterNames.forEach(name => {
   console.log(name);
