29cbba20f2c18cf046219ff6b2bb7d528276e37e
Arion <ccina0156@gmail.com>
2024-06-13 20:30:18 +0200

    Refactorizar CharacterProfileManager.js

--- Scripts/CharacterProfileManager.js
+++ Scripts/CharacterProfileManager.js
@@ -6,7 +6,6 @@ class CharacterProfileManager {
     this.characters = [];
   }
 
-  // Método para inicializar la carga de personajes y configurar el evento del selector
   async init() {
     await this.loadCharacterFolders();
     if (this.characters.length > 0) {
@@ -15,53 +14,34 @@ class CharacterProfileManager {
     this.setupSelectEvent();
   }
 
-  // Método para cargar las carpetas de personajes desde el repositorio
   async loadCharacterFolders() {
     const response = await fetch(this.repoUrl);
     const folders = (await response.json()).filter(item => item.type === 'dir');
     this.characters = folders.map(folder => folder.name);
-
     this.populateSelectElement();
   }
 
-  // Método para llenar el elemento select con los nombres de los personajes
   populateSelectElement() {
     this.characters.forEach(characterName => {
       this.selectElement.add(new Option(characterName, characterName));
     });
   }
 
-  // Método para configurar el evento change del elemento select
   setupSelectEvent() {
     this.selectElement.addEventListener('change', ({ target: { value } }) => {
       this.loadCharacterContent(value);
     });
   }
 
-  // Método para cargar el contenido de un personaje específico
   async loadCharacterContent(characterName) {
     const characterContentUrl = `https://arion-j.github.io/Novel/Personajes/${characterName}/`;
     const contentResponse = await fetch(characterContentUrl);
     this.resumeElement.innerHTML = await contentResponse.text();
   }
 
-  // Método para obtener solo los nombres de los personajes
   getCharacterNames() {
     return this.characters;
   }
 }
 
-// Exportar la función para obtener los nombres de los personajes
-export function getCharacterNames() {
-  const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes';
-  const characterProfileManager = new CharacterProfileManager('selectpj', 'resumepj', repoUrl);
-  characterProfileManager.init();
-  return characterProfileManager.getCharacterNames();
-}
-
-// Asegurarse de que el DOM esté completamente cargado antes de inicializar la clase
-document.addEventListener('DOMContentLoaded', () => {
-  const repoUrl = 'https://api.github.com/repos/arion-j/Novel/contents/Personajes';
-  const characterProfileManager = new CharacterProfileManager('selectpj', 'resumepj', repoUrl);
-  characterProfileManager.init();
-});
+export default CharacterProfileManager;
